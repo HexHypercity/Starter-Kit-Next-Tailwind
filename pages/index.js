c451a8f1f8c3ea0cd694581f9c5e6d26896dabc7
Raiyan Siraj Akib <developerraiyan@gmail.com>
2021-05-12 13:38:46 +0600

    Initial Kit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="w-screen h-screen bg-indigo-500 text-white">
+      <h1 className="flex h-full w-full text-3xl justify-center items-center">
+        Hello World
+      </h1>
+    </div>
+  );
+}
